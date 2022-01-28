44b194a71bdb1a7d81f2211c7cbb755e6742615d
David <davskim@umich.edu>
2022-01-28 15:04:52 -0500

    DavidEIRatio
    Added the function DavidEIRatio which is used for plotting EI Ratio with streamlined channel selections

---
+++ SpikingAnalysis/DavidEIRatio.m
@@ -0,0 +1,204 @@
+function EIRatioData = EIRatio(basepath,binwidthsecs,smoothingnumpoints)
+% EI is pure ratio of E num spikes/ I num spikes per bin
+% ZEI is zscored version of that
+% PCEI is ratePerECell/ratePerICell (normalized by number of cells)
+% ZPCEI is zscored version of that - gives best comparability across
+% sessions
+% er and ir are E and I rates within each bin for total population
+% numEcells, numIcells: self-explanatory
+
+%% Constants
+if ~exist('binwidthsecs','var')
+    binwidthsecs = 5;
+end
+if ~exist('smoothingnumpoints','var')
+    smoothingnumpoints = 1;
+end
+sampfreq = 1;
+plotting = 1;
+savingfigs = 1;
+
+%% Variables and Loading
+if ~exist('basepath','var')
+    basepath = cd;
+end
+basename = bz_BasenameFromBasepath(basepath);
+
+spikes = bz_GetSpikes('basepath',basepath);
+CellClass = bz_LoadCellinfo(basepath,'CellClass');
+states = bz_LoadStates(basepath,'SleepState');
+ints = states.ints;
+
+numEcells = sum(CellClass.pE);
+numIcells = sum(CellClass.pI);
+
+% load(fullfile(basepath,[basename '_SSubtypes.mat']))
+% 
+% if ~exist('Se','var') & exist('Se_TsdArrayFormat');
+%     Se = Se_TsdArrayFormat;
+% end
+% if ~exist('Si','var') & exist('Si_TsdArrayFormat');
+%     Si = Si_TsdArrayFormat;
+% end
+% 
+% numEcells = length(Se);
+% numIcells = length(Si);
+% if numIcells<3
+%     return
+% end
+% load(fullfile(basepath,[basename '_GoodSleepInterval.mat']))
+% gsr = StartEnd(GoodSleepInterval,'s');
+
+
+%% Generate bins
+% if numIcells>0
+%     maxbin = min([max(TimePoints(oneSeries(Se),'s')) max(TimePoints(oneSeries(Si),'s'))]);%because of Inf as max of some Good Sleeps
+% else
+%     maxbin = max(TimePoints(oneSeries(Se),'s'));%because of Inf as max of some Good Sleeps
+% end
+% binstartends = 0:binwidthsecs:maxbin;
+% binstartends(end+1) = binstartends(end)+binwidthsecs;
+% if binstartends(end) == binstartends(end-1)
+%     binstartends(end) = [];
+% end
+
+load('goodUnitsCurated.mat');
+load('goodUnitsDaviolin.mat');
+[lol,hah,goodUnitsBoth] = dummyCompare(goodUnits,goodUnitsDaviolin);
+ sieve = {goodUnits,goodUnitsDaviolin,goodUnitsBoth};
+            
+            
+for s = 1:1%length(sieve)
+    subsieve = sieve{3};
+    conservedSpikes = spikes;
+    conservedCellClass = CellClass;
+    for p = flip(1:length(spikes.UID))
+        if ~ismember(p,subsieve)
+           spikes.UID(p) = [];
+           spikes.times(p) = [];
+           spikes.cluID(p) = [];
+           spikes.rawWaveform(p) = [];
+           spikes.maxWaveformCh(p) = [];
+           CellClass.UID(p) = [];
+           CellClass.pE(p) = [];
+           CellClass.pI(p) = [];
+           CellClass.label(p) = [];
+        end
+    end
+end
+
+[spikemat] = bz_SpktToSpkmat(spikes, 'binsize',binwidthsecs,'dt',binwidthsecs);
+bincenters = spikemat.timestamps;
+binstarts = bincenters-(binwidthsecs*0.5);
+binends = bincenters+(binwidthsecs*0.5);
+
+% binstartends = binstartends * sampfreq;
+% binInts = intervalSet(binstartends(1:end-1),binstartends(2:end));
+
+%% Bin and Divide to get EIRatio (non-normalized)
+er = spikemat.data(:,CellClass.pE);
+er = sum(er,2);
+
+% er = MakeQfromTsd(oneSeries(Se),binInts);
+% er = sum(Data(er),2);
+er = er./binwidthsecs;
+
+
+
+% er = er./numEcells;
+if numIcells > 0
+    ir = spikemat.data(:,CellClass.pI);
+    ir = sum(ir,2);
+
+    % er = MakeQfromTsd(oneSeries(Se),binInts);
+    % er = sum(Data(er),2);
+    ir = ir./binwidthsecs;
+else
+    ir = nan(size(ir,1),1);
+end
+
+EI = er./(er+ir); %te/(te+ti)
+PCEI = (er/numEcells) ./ ((er/numEcells) + (ir/numIcells));
+ZEI = nanzscore(EI);
+ZPCEI = nanzscore(PCEI);
+
+%% Smooth
+EI = smooth(EI,smoothingnumpoints);
+ZEI = smooth(ZEI,smoothingnumpoints);
+PCEI = smooth(PCEI,smoothingnumpoints);
+ZPCEI = smooth(ZPCEI,smoothingnumpoints);
+er = smooth(er,smoothingnumpoints);
+ir = smooth(ir,smoothingnumpoints);
+
+%% Plot
+if plotting
+%     load(fullfile(basepath,[basename '_Intervals.mat']))
+%     load(fullfile(basepath,[basename '_Motion.mat']))
+%     m = tsd(10000*[1:length(motiondata.motion)]',motiondata.motion');
+%     m = tsdArray(m);
+%     m = MakeSummedValQfromS(m,binInts);
+%     m = Data(m);
+%     m(isnan(m))=0;
+%     m = smooth(m,smoothingnumpoints);
+    if exist(fullfile(basepath,[basename '.EMGFromLFP.LFP.mat']))
+        load(fullfile(basepath,[basename '.EMGFromLFP.LFP.mat']))
+        m = EMGFromLFP.data;
+    elseif exist(fullfile(basepath,[basename '_EMGCorr.mat']))
+        load(fullfile(basepath,[basename '_EMGCorr.mat']))
+        m = tsd(EMGCorr(:,1),EMGCorr(:,2));
+    else
+        load(fullfile(basepath,[basename '.eegstates.mat']))
+        m = StateInfo.motion;
+        m = tsd(1:length(m),m);
+    end
+%     m = tsdArray(m);
+%     m = MakeSummedValQfromS(m,binInts);
+%     m = Data(m);
+%     m(isnan(m))=0;
+%     m = smooth(m,smoothingnumpoints);
+    m = ResampleTolerant(m,length(bincenters),length(m));
+
+%     figname = [basename '_EIRatioBin' num2str(binwidthsecs) 'Smooth' num2str(smoothingnumpoints) 'Norm' normstring];
+    figname = [basename '_EIRatioBin' num2str(binwidthsecs) 'Smooth' num2str(smoothingnumpoints)];
+    h = figure('position',[100 100 600 600],'name',figname);
+    subplot(4,1,1)
+        plot(bincenters,EI,'k')
+        hold on
+        axis tight
+        plotIntervalsStrip(gca,ints,1)
+        yl = ylim;
+        plot([yl(2)*.75 yl(2)*.75],'-.','color',[.5 .5 .5])
+        axis tight
+        ylabel('EIRatio')
+        title(['E/I Ratio. Binning:' num2str(binwidthsecs) 'sec. Smooth By: ' num2str(smoothingnumpoints)  'points. File:' basename '.'],'fontsize',8,'fontweight','normal')
+    subplot(4,1,2)
+        plot(bincenters,er,'b')
+        hold on
+        axis tight
+        yl = ylim;
+        plot([yl(2)*.75 yl(2)*.75],'-.','color',[.5 .5 .5])
+        ylabel('E Rate(Hz)')
+    subplot(4,1,3)
+        plot(bincenters,ir,'r')
+        hold on
+        axis tight
+        yl = ylim;
+        plot([yl(2)*.75 yl(2)*.75],'-.','color',[.5 .5 .5])
+        ylabel('I Rate(Hz)')
+    subplot(4,1,4)
+        plot(bincenters,m,'color',[.4 .4 .4])
+        hold on
+        axis tight
+        yl = ylim;
+        plot([yl(2)*.75 yl(2)*.75],'-.','color',[.5 .5 .5])
+        ylabel('Motion')
+%% save figure
+    if savingfigs
+        savedir = fullfile(basepath,'Figures');
+        MakeDirSaveFigsThereAs(savedir,h,'fig')
+        MakeDirSaveFigsThereAs(savedir,h,'png')
+    end
+end
+
+EIRatioData = v2struct(EI,ZEI,PCEI,ZPCEI,er,ir,binwidthsecs,bincenters,smoothingnumpoints,numEcells,numIcells);
+save(fullfile(basepath,[basename '_EIRatio_Bin' num2str(binwidthsecs) 'Smooth' num2str(smoothingnumpoints) '.mat']),'EIRatioData')
