d91de20a375edafcbf9cb25f40b9e5aa1575f311
David <davskim@umich.edu>
2022-03-04 13:39:52 -0500

    filtered Converting bzspikes to csv
    exactly what it sounds like lol. This is gonna be useful for GLMCC and connect! Except now it has some filtering functionality that EIRatio and HRLRRatio has.

--- SpikingAnalysis/bzspikestocsv.m
+++ SpikingAnalysis/bzspikestocsv.m
@@ -1,17 +1,58 @@
 %bzspikes to csv...
-for i = 1:length(spikes.times)
-    sizes(i) = length(spikes.times{i});
+%requires a spikes file to be loaded already... for now at least...
+%Along with that, it requires the times variable to be loaded... lol...
+
+filtered = 1;
+nicvar = 1;
+timeslice = 1;
+[parentpath,basename] = fileparts(cd);
+basepath = [parentpath,'/', basename];
+
+if filtered 
+   load([basepath, '/goodUnitsCurated.mat']);
+   load([basepath, '/goodUnitsDaviolin.mat']); 
+   load([basepath, '/', basename, '_InjectionComparisionIntervals.mat']);
+   [lol,hah,goodUnitsBoth] = UnitsCompare(goodUnits,goodUnitsDaviolin);
+   allUnits = 1:length(spikes.UID);
+   sieve = {allUnits,goodUnits,goodUnitsDaviolin,goodUnitsBoth};
+   sievenames = {'All Units','Max Choices', 'David Choices', 'Collab Choices'};
 end
 
-csv = array2table(NaN(max(sizes),length(spikes.times)));
 
-for j = 1:length(spikes.times)
-    csv(1:length(spikes.times{1,j}),j) = array2table(spikes.times{1,j});
+for s = 1:length(sieve)
+    if filtered
+        subsieve = sieve{s};
+        conservedSpikes = spikes;
+        for p = flip(1:length(spikes.UID))
+            if ~ismember(p,subsieve)
+               spikes.times(p) = [];
+            end
+        end
+    end
+    
+    for i = 1:length(spikes.times)
+        sizes(i) = length(spikes.times{i});
+    end
+
+    csv = array2table(NaN(max(sizes),length(spikes.times))); %creating NaN table that will be csv
+
+    for j = 1:length(spikes.times)
+        csv(1:length(spikes.times{1,j}),j) = array2table(spikes.times{1,j}); %transferring spike data into csv
+    end
+    
+    if timeslice %if we're gonna pick and choose csvs based on timeframes
+        newcsv = csv;
+        
+    end
+    
+    
+    %this is just for the Reformatting function that Nicolette's data uses
+    if nicvar
+        allfilestartcol = array2table(NaN(max(sizes),1));
+        allfilestartcol.Properties.VariableNames = {'AllFile_start'};
+        csv = [csv allfilestartcol];
+    end
+
+    writetable(csv, [basename,'_',sievenames{s},'.csv']);
+    spikes = conservedSpikes;
 end
-
-%this is just for the Reformatting function that Nicolette's data uses
-allfilestartcol = array2table(NaN(max(sizes),1));
-allfilestartcol.Properties.VariableNames = {'AllFile_start'};
-csv = [csv allfilestartcol];
-
-writetable(csv, 'Dino_080114.csv');
